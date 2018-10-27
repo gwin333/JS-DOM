1d45604b5937e61b83b82f462f2e78de92565c2a
gwin333 <yang.290@gmail.com>
2018-10-27 09:02:12 -0500

    Exercises 1 to 4

--- ex1/script.js
+++ ex1/script.js
@@ -1,1 +1,4 @@
 // Change the text of the h1 tag to "I have changed the text"
+
+var element = document.getElementById("title");
+element.innerHTML = "I have changed the text";
--- ex2/script.js
+++ ex2/script.js
@@ -1,1 +1,5 @@
 // Change the background color of the box to blue
+
+var element1=document.getElementById("box");
+// element1.style.bgcolor="3500FF";
+element1.style.backgroundColor="#3500FF";
