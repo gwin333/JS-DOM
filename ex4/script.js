3ebaf415dcc4361f54aed2c409c2d9da406fb6ae
gwin333 <yang.290@gmail.com>
2018-11-08 21:30:05 -0600

    Exercise 4

--- ex4/script.js
+++ ex4/script.js
@@ -1,7 +1,28 @@
 // Make all of the boxes blue when you click the button
 // Notice that all the boxes have "class" instead of "id"
 
+// var divs = document.getElementsByClassName("box");
+// console.log(divs);
+// console.log(divs[0]);
+
+
+// divs[0].style.backgroundColor="blue";
+// divs.style.backgroundColor="blue"; GIVES ERROR 
+
+// var button = document.getElementById("myButton");
+
+// button.addEventListener('click', function() {
+//   document.getElementById('box').style.backgroundColor = 'blue'
+// })
+
 var button = document.getElementById("myButton");
-button.addEventListener("click", function() {
-    document.getElementsByClassName("box").style.backgroundColor="blue";
-})
+
+console.log(divs);
+var divs = document.querySelectorAll('.box'); 
+
+button.addEventListener('click',function(){
+
+for(var i = 0; i < divs.length, i++){
+    divs[i].style.backgroundColor = 'blue'
+}
+)}
